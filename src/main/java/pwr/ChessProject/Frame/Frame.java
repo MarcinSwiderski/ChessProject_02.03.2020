b70f750cf6d3cbb688a3c910686ddd5e1c217e56
MarcinSwiderski <marswider@gmail.com>
2020-03-03 00:45:21 +0100

    Implementing buttons in Frame

--- src/main/java/pwr/ChessProject/Frame/Frame.java
+++ src/main/java/pwr/ChessProject/Frame/Frame.java
@@ -17,10 +17,32 @@ public class  Frame extends JFrame {
         int heightOfScreen = Toolkit.getDefaultToolkit().getScreenSize().height; // pobiera wysokość ekranu
         int widthOfScreen = Toolkit.getDefaultToolkit().getScreenSize().width;  // pobiera szerokość ekranu
         this.setBounds(300, 300, 600, 400); // lokalizacja i wielkość okienka
-        this.setIconImage(Toolkit.getDefaultToolkit().getImage("pawelek.jpg")); // ikonka w lewym górnym rogu
+        // this.setIconImage(Toolkit.getDefaultToolkit().getImage("pawelek.jpg")); // ikonka w lewym górnym rogu
         this.setDefaultCloseOperation(3); // Dla głównego okienka najlepiej 3, Dla komponentów 2
         this.setTitle("ChessProject");
-        initComponentsLayout(); // funkcja z ułożonymi guzikami
+        initComponentsLayout(); // funkcja z ułożonymi guzikamiw
+        bDecline.addActionListener(e -> {
+            this.hide();
+        });
+        /*
+        chooseFigureTextField.addActionListener(e -> {
+            if(chooseFigureTextField.getText().toString().length() < 3 )
+            switch(chooseFigureTextField.getText())
+            {
+                case 'P' :
+                    return "A";
+                case 'B' :
+                    return "B";
+                case 'R' :
+                    return "R";
+                case 'Q' :
+                    return "Q";
+
+            }
+        });
+        */
+
+
     }
 
     public void initComponentsLayout() {
@@ -37,40 +59,67 @@ public class  Frame extends JFrame {
         layout.setHorizontalGroup(
                 layout.createSequentialGroup()
                         .addGroup(
-                                layout.createParallelGroup().addComponent(button1, 10 , GroupLayout.DEFAULT_SIZE, 300).addComponent(colorRed, 10 , GroupLayout.DEFAULT_SIZE, 300) // dodawanie guzika (nazwa, minimalna szerokość, szerokość podczas stworzenia, maksymalna wielkość)
+                                layout.createParallelGroup().addComponent(labelFromXAxis, 10 , 200, 300).addComponent(xAxisFromTextField, 10 , 15, 40).addComponent(labelToXAxis, 10 , 200, 300).addComponent(xAxisToTextField, 10 , 15, 40) // dodawanie guzika (nazwa, minimalna szerokość, szerokość podczas stworzenia, maksymalna wielkość)
 
                         )
                         .addGroup(
-                                layout.createParallelGroup().addComponent(button2, 10 , GroupLayout.DEFAULT_SIZE, 300).addComponent(button4, 10 , GroupLayout.DEFAULT_SIZE, 300)
+                                layout.createParallelGroup().addComponent(labelFromYAxis, 10 , 200, 300).addComponent(yAxisFromTextField, 10 , 15, 40).addComponent(labelToYAxis, 10 , 200, 300).addComponent(yAxisToTextField, 10 , 15, 40) // dodawanie guzika (nazwa, minimalna szerokość, szerokość podczas stworzenia, maksymalna wielkość)
+
                         )
-                        .addComponent(button3, 10 , GroupLayout.DEFAULT_SIZE, 300)
-                        .addContainerGap(5, Short.MAX_VALUE)
+                        .addGroup(
+                                layout.createParallelGroup().addComponent(bAcceptMove)
+                        )
+                        .addContainerGap(30, Short.MAX_VALUE)
                         .addComponent(bDecline)
         );
         layout.setVerticalGroup(
                 layout.createSequentialGroup()
                         .addGroup(
-                                layout.createParallelGroup().addComponent(button1, 10 , GroupLayout.DEFAULT_SIZE, 200).addComponent(button2, 10 , GroupLayout.DEFAULT_SIZE, 200).addComponent(button3, 10 , GroupLayout.DEFAULT_SIZE, 200)
+                                layout.createParallelGroup().addComponent(labelFromXAxis, 10 , 15, 40).addComponent(labelFromYAxis, 10 , 15, 40)
                         )
                         .addGroup(
-                                layout.createParallelGroup().addComponent(button4, 10 , GroupLayout.DEFAULT_SIZE, 200).addComponent(colorRed, 10 , GroupLayout.DEFAULT_SIZE, 300)
+                                layout.createParallelGroup().addComponent(xAxisFromTextField, 10 , 15, 40).addComponent(yAxisFromTextField, 10 , 15, 40)
                         )
-                        .addContainerGap(5, Short.MAX_VALUE)
+                        .addGroup(
+                                layout.createParallelGroup().addComponent(labelToXAxis, 10 , 15, 40).addComponent(labelToYAxis, 10 , 15, 40)
+                        )
+                        .addGroup(
+                                layout.createParallelGroup().addComponent(xAxisToTextField, 10 , 15, 40).addComponent(yAxisToTextField, 10 , 15, 40).addComponent(bAcceptMove)
+                        )
+                        .addContainerGap(30, Short.MAX_VALUE)
                         .addComponent(bDecline)
         );
-        //pack(); //ustala wielkość okienka co do domyślnych rozmiarów buttonów
+        pack(); //ustala wielkość okienka co do domyślnych rozmiarów buttonów
 
     }
     // Tworzenie panelu
     JPanel panel1 = new JPanel(new FlowLayout(FlowLayout.TRAILING));
 
-    // DEKLAROWANIE (TWORZENIE GUZIKÓW)
-    JButton button1 = new JButton("button1");
-    JButton button2 = new JButton("button2");
-    JButton button3 = new JButton("button3");
-    JButton button4 = new JButton("button4");
+    // DEKLAROWANIE
+    JButton bAcceptMove = new JButton("Accept!");
     JButton bDecline = new JButton("Decline");
-    JButton colorRed = new JButton("Red");
-    // JTextField
+
+    // "From" Labels and TextFields
+
+    JTextField xAxisFromTextField = new JTextField("A");
+    JTextField yAxisFromTextField = new JTextField("A");
+     //JTextField chooseFromFigureTextField = new JTextField("A");
+     //JLabel labelFromFigureName = new JLabel("Type below figure short-name!");
+    JLabel labelFromXAxis = new JLabel("Type below XAxis of position");
+    JLabel labelFromYAxis = new JLabel("Type below YAxis of position");
+
+    // "To" Labels and TextFields
+
+    JTextField xAxisToTextField = new JTextField("B");
+    JTextField yAxisToTextField = new JTextField("B");
+     //JTextField chooseToFigureTextField = new JTextField("B");
+     //JLabel labelToFigureName = new JLabel("TO below figure short-name!");
+    JLabel labelToXAxis = new JLabel("TO below XAxis of position");
+    JLabel labelToYAxis = new JLabel("TO below YAxis of position");
+
+    // Image
+    ImageIcon icon = new ImageIcon("");
+
+
 
 }
