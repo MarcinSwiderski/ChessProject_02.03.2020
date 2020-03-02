6b2ee4275f34a8641ab7ec46808f73ea4546e07b
MarcinSwiderski <marswider@gmail.com>
2020-03-02 22:03:12 +0100

    Create window

--- src/main/java/pwr/ChessProject/Main.java
+++ src/main/java/pwr/ChessProject/Main.java
@@ -1,7 +1,10 @@
 package pwr.ChessProject;
 
+import pwr.ChessProject.Frame.Frame;
+
 public class Main {
     public static void main(String[] p ) {
         System.out.println("xd");
+        Frame.frame();
     }
 }
