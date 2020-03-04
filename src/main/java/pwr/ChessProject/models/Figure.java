77c33c90dee0a344f9948ee44682770c8eb31a95
PawelKowalczuk00 <56939879+PawelKowalczuk00@users.noreply.github.com>
2020-03-04 12:17:48 +0100

    Todo

--- src/main/java/pwr/ChessProject/models/Figure.java
+++ src/main/java/pwr/ChessProject/models/Figure.java
@@ -21,4 +21,6 @@ public abstract class Figure {
     public Figure(Player player) {
         this.player = player;
     }
+
+    // TODO: 04.03.2020 Actual movement and killing actions including king specials 
 }
