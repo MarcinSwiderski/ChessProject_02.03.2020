088eec3a766b4967076908d8448384d503f8a7e7
PawelKowalczuk00 <56939879+PawelKowalczuk00@users.noreply.github.com>
2020-03-24 16:04:15 +0100

    Fixed Board tests

--- src/test/java/pwr/chessproject/game/BoardTest.java
+++ src/test/java/pwr/chessproject/game/BoardTest.java
@@ -27,10 +27,10 @@ class BoardTest {
     }
 
     @ParameterizedTest
-    @CsvSource({"-1, 8", "64, 20"})
-    void selectingFigureOutsideOfRangeThrowsException(int position, int target) {
+    @CsvSource({"-1", "64"})
+    void selectingFigureOutsideOfRangeThrowsException(int position) {
         Board board = new Board();
-        Exception exception = assertThrows(IllegalArgumentException.class, ()-> board.moveFigure(position, target));
+        Exception exception = assertThrows(IllegalArgumentException.class, ()-> board.checkPosition(position));
 
         String expectedMessage = "Can not select figure outside of the board";
         String actualMessage = exception.getMessage();
@@ -42,7 +42,7 @@ class BoardTest {
     @Test
     void selectBlankFieldThrowsException() {
         Board board = new Board();
-        Exception exception = assertThrows(NullPointerException.class, ()-> board.moveFigure(32, 31));
+        Exception exception = assertThrows(NullPointerException.class, ()-> board.checkPosition(32));
 
         String expectedMessage = "There is no figure at the selected position";
         String actualMessage = exception.getMessage();
